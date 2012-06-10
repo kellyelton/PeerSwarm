c6a97a102e839007b9881a37b6c55937025ca23b
kellyelton <kelly.elton@skylabsonline.com>
2012-06-09 21:36:06 -0500

    Should function

--- dhttest/Main.cs
+++ dhttest/Main.cs
@@ -1,12 +1,38 @@
 using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Threading;
+using MonoTorrent;
+using MonoTorrent.Dht;
+using MonoTorrent.Dht.Listeners;
+using MonoTorrent.Tracker.Listeners;
 
 namespace dhttest
 {
-	class MainClass
+	public class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			var listener = new DhtListener(new IPEndPoint(IPAddress.Any , 15000));
+			var engine = new DhtEngine(listener);
+			SHA1 sha = new SHA1CryptoServiceProvider();
+			var hash = new InfoHash(sha.ComputeHash(System.Text.Encoding.ASCII.GetBytes("OCTGN")));
+
+			byte[] nodes = null;
+			if (File.Exists("mynodes"))
+				nodes = File.ReadAllBytes("mynodes");
+
+			engine.PeersFound += (o, e) => Console.WriteLine("I FOUND PEERS: {0}", e.Peers.Count);
+			listener.Start();
+			engine.Start(nodes);
 			
+			while(Console.ReadLine() != "q")
+			{
+				engine.GetPeers(hash);
+				Thread.Sleep(100);
+			}
+			File.WriteAllBytes("mynodes", engine.SaveNodes());
 		}
 	}
 }
